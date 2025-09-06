f4b4556e3fc5993bb3491ec7cf551bb06f8d5148
AmiahRaine <amiah.belleisle@proton.me>
2025-09-05 20:17:22 -0500

    Formatting for BigDecimal

--- back-end/budget-tracker/src/main/java/dev/amiah/budget_tracker/dto/TotalBalanceObject.java
+++ back-end/budget-tracker/src/main/java/dev/amiah/budget_tracker/dto/TotalBalanceObject.java
@@ -1,6 +1,8 @@
 package dev.amiah.budget_tracker.dto;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class TotalBalanceObject {
 
@@ -20,7 +22,7 @@ public class TotalBalanceObject {
     public BigDecimal getTotal() {
         // Return 0 instead of null
         if (total != null) {
-            return total;
+            return total.setScale(2, RoundingMode.HALF_UP);
         }
         return BigDecimal.ZERO;
     }
